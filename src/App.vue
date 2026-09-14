cb0c817aa4c0e8d2d4e805d439b37f7132dc1f98
Cheng <Chengchenghu0728@gmail.com>
2026-09-14 19:38:40 +0800

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { Button } from "@/components/ui/button";
+</script>
+
+<template>
+  <main
+    class="grid min-h-screen place-items-center bg-background text-foreground">
+    <div class="space-y-4 text-center">
+      <h1 class="text-3xl font-bold">Vue 3 + Tailwind CSS + shadcn-vue</h1>
+      <Button>Start</Button>
+    </div>
+  </main>
+</template>
